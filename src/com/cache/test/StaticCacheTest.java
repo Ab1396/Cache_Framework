2e0e0f2b4d62f13a6869db3877cce0963a01067c
Sanganagouda <sanjubharamagoudra143@gmail.com>
2018-02-05 11:51:30 +0530

    added StaticCache class and test files.

---
+++ src/com/cache/test/StaticCacheTest.java
@@ -0,0 +1,39 @@
+package com.cache.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StaticCacheTest {
+
+	@Test
+	public void testReadCache() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testRemoveCache() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testInsertCache() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testUpdateCache() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testDisplayCache() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testClearCache() {
+		assertTrue(true);
+	}
+
+}
