97081ba779e06191be5549674707011f841d5593
vailaya <vailaya@EABXZB5W5J.asiapacific.hpqcorp.net>
2018-03-05 17:32:30 +0530

    Filesystem Creation and Updated code

--- src/com/store/filesystem/Filesystem.java
+++ src/com/store/filesystem/Filesystem.java
@@ -3,18 +3,66 @@ package com.store.filesystem;
 import java.util.List;
 
 import com.store.Store;
+import java.io.*;
 
-public class Filesystem implements Store{
+public class Filesystem implements Store
+{
 
 	@Override
-	public boolean openconnection() {
+	public boolean openconnection() 
+	{
 		// TODO Auto-generated method stub
 		return true;
+	}
+	
+
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("File system running");
+		
+		
+		// Creating a new RandomAccessFile - "GEEK"
+		RandomAccessFile f1 = new RandomAccessFile("C:\\Users\\vailaya\\Desktop\\file.txt", "rw");
+		System.out.print("File Created");
+		
+		
+		
+		// Writing to file
+        f1.writeUTF("File written");
+        System.out.print("File Written\n");
+        
+     // read() method :
+        System.out.println("Use of read() method : " + f1.read());
+        
+		//try 1:
+		/*String fileName = "C:\\Users\\vailaya\\Desktop\\source.txt";
+	    File fileObject = new File(fileName);
+
+	    if (!fileObject.exists()) 
+	    {
+	    	System.out.println(fileName);
+	    	initialWrite(fileName);
+	    }
+	    //readFile(fileName);
+	    //readFile(fileName);*/
+		
+		
+		
+		/*File f;
+		  f=new File("C:\\Users\\vailaya\\Desktop\\myfile.txt");
+		  if(!f.exists())
+		  {
+			  f.createNewFile();
+			  System.out.println("New file \"myfile.txt\" has been created to the current directory");
+		  }*/
 	}
 
 	@Override
 	public boolean insert(String key, String value) {
 		// TODO Auto-generated method stub
+		
+		
+		
 		return true;
 	}
 
